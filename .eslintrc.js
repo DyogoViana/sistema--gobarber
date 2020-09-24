0696ac0edccb1b93362af0bb4d722811edd45b68
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-09-24 12:33:03 -0300

    [BUG] : OK, falta ajuste.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,34 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 11,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: [
+		"plugin:react/recommended",
+		"airbnb",
+		"prettier",
+		"prettier/react",
+	],
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly",
+	},
+	parser: "babel-eslint",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: "module",
+	},
+	plugins: ["react", "prettier", "react-hooks"],
+	rules: {
+		"prettier/prettier": "error",
+		"react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
+		"import/prefer-default-export": "off",
+		"no-param-reassign": "off",
+		"no-console": ["error", { allow: ["tron"] }],
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+	},
 };
--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import './config/ReactotronConfig';
-
 import React from 'react';
-import { Router } from 'react-router-dom';
-
-import Routes from './routes';
-import history from './services/history';
 
 function App() {
-  return (
-    <Router history={history}>
-      <Routes />
-    </Router>
-  );
+	return <div className="App" />;
 }
 
 export default App;
