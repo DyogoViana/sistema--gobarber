94be79772778f2a37e5ba0b4947ca4c96b5d34b2
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-10-09 16:10:03 -0300

    Rotas privadas [erro].

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './config/ReactotronConfig';
-
 import React from 'react';
 import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
 
 import Routes from './routes';
 import history from './services/history';
 
 function App() {
-	return (
-		<Router history={history}>
-			<Routes />
-		</Router>
-	);
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
 
 export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SingIn from '../pages/SignIn';
 import SingUp from '../pages/SignUp';
@@ -8,13 +9,13 @@ import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 
 export default function Routes() {
-	return (
-		<Switch>
-			<Route path="/" exact component={SingIn} />
-			<Route path="/register" component={SingUp} />
+  return (
+    <Switch>
+      <Route path="/" exact component={SingIn} />
+      <Route path="/register" component={SingUp} />
 
-			<Route path="/dashboard" component={Dashboard} />
-			<Route path="/profile" component={Profile} />
-		</Switch>
-	);
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+    </Switch>
+  );
 }
