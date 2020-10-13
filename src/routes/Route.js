e35c96fbe3d208b9801438dbbe8eba9bc6b90476
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-10-13 19:24:40 -0300

    Rotas para os layouts para usuários logados e não logados.

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false; // Controla de forma manual a entrada do usuário (logIn).
+  const signed = true; // Controla de forma manual a entrada do usuário (logIn).
 
   // Redireciona o usuário de volta ao login, caso não esteja logado.
   if (!signed && isPrivate) {
@@ -19,7 +22,19 @@ export default function RouteWrapper({
     return <Redirect to="/dashboard" />;
   }
 
-  return <Route {... rest} component={Component} />;
+  // Escolhe o layout de acordo com o acesso do usuário.
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {... rest}
+      render={(props) => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
