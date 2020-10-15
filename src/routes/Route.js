287b8aa88e450dfb9ab11c1e10c670ec7bc6a2fa
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-10-15 16:31:14 -0300

    [Ajuste]

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -6,42 +6,43 @@ import AuthLayout from '../pages/_layouts/auth';
 import DefaultLayout from '../pages/_layouts/default';
 
 export default function RouteWrapper({
-  component: Component,
-  isPrivate,
-  ...rest
+	component: Component,
+	isPrivate,
+	...rest
 }) {
-  const signed = false; // Controla de forma manual a entrada do usuário (logIn).
+	const signed = false; // Controla de forma manual a entrada do usuário (logIn).
 
-  // Redireciona o usuário de volta ao login, caso não esteja logado.
-  if (!signed && isPrivate) {
-    return <Redirect to="/" />;
-  }
+	// Redireciona o usuário de volta ao login, caso não esteja logado.
+	if (!signed && isPrivate) {
+		return <Redirect to="/" />;
+	}
 
-  // Estando logado, o usuário entra na página de dashboard.
-  if (signed && !isPrivate) {
-    return <Redirect to="/dashboard" />;
-  }
+	// Estando logado, o usuário entra na página de dashboard.
+	if (signed && !isPrivate) {
+		return <Redirect to="/dashboard" />;
+	}
 
-  // Escolhe o layout de acordo com o acesso do usuário.
-  const Layout = signed ? DefaultLayout : AuthLayout;
+	// Escolhe o layout de acordo com o acesso do usuário.
+	const Layout = signed ? DefaultLayout : AuthLayout;
 
-  return (
-    <Route
-      {... rest}
-      render={(props) => (
-        <Layout>
-          <Component {...props} />
-        </Layout>
-      )}
-    />
-  );
+	return (
+		<Route
+			{...rest}
+			render={(props) => (
+				<Layout>
+					<Component {...props} />
+				</Layout>
+			)}
+		/>
+	);
 }
 
 RouteWrapper.propTypes = {
-  isPrivate: PropTypes.bool,
-  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired,
+	isPrivate: PropTypes.bool,
+	component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+		.isRequired,
 };
 
 RouteWrapper.defaultProps = {
-  isPrivate: false,
+	isPrivate: false,
 };
